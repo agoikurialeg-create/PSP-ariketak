1d5f36f5db0415bad4095555b0d5cb138cc56688
2AM3-3 <2AM3-3@ORO201083-2AM33.zornotzalh.eus>
2025-09-19 09:14:04 +0200

    Itxaron bukatuta

--- PSP_Ariketak/src/gaia1_ariketa3/Itxaron.java
+++ PSP_Ariketak/src/gaia1_ariketa3/Itxaron.java
@@ -2,9 +2,11 @@ package gaia1_ariketa3;
 
 public class Itxaron {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws NumberFormatException, InterruptedException {
+		long pid = ProcessHandle.current().pid();
+		System.out.println("Nire PID da: " + pid + " - Sarrerak: " + args[0]);
+		Thread.sleep(Integer.valueOf(args[0])*1000);
+		System.out.println("Nire PID da: " + pid + " - Bukatu da");
 	}
 
 }
--- PSP_Ariketak/src/gaia1_ariketa3/ItxaronAbiarazlea.java
+++ PSP_Ariketak/src/gaia1_ariketa3/ItxaronAbiarazlea.java
@@ -1,16 +1,21 @@
 package gaia1_ariketa3;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class ItxaronAbiarazlea {
 	public static void main(String[] args) {
 		try {
+			
 			// Zehaztu classpath-a ("bin" Eclipse erabiltzean adibidez, moldatu beharrrezkoa bada)
 			String classpath = "bin"; // `.` karpeta berdinean bada
+			String[] segunduak = args[0].split(",");
+			for (String param : segunduak) {
 
 			// Batuketa klasea abiarazi, paketea zehaztuz
 			ProcessBuilder processBuilder_1 = new ProcessBuilder("java", "-cp", classpath,
-					"prozesuAnitzekoProgramazioa_1.Batuketa", "6", "10");
+					"gaia1_ariketa3.Itxaron", param);
+			
 
 			// Sarrera eta irteera jarauntsi (heredar)
 			processBuilder_1.inheritIO();
@@ -19,10 +24,11 @@ public class ItxaronAbiarazlea {
 			Process process_1 = processBuilder_1.start();
 
 			// Itxaron prozesua bukatu arte
-			int exitCode_1 = process_1.waitFor();
-			System.out.println("\n 1 Prozesua bukatu da irteera kode honekin: " + exitCode_1);
+			//int exitCode_1 = process_1.waitFor();
+			//System.out.println("\n 1 Prozesua bukatu da irteera kode honekin: " + exitCode_1);
+			}
 
-		} catch (IOException | InterruptedException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
